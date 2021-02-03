3f5b88ac116912cfae07de952b93a2e5b26f199c
Hunter <50486856+kalinchakma@users.noreply.github.com>
2021-02-04 05:22:25 +0600

    Update header.php

--- views/header footer/header.php
+++ views/header footer/header.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Free Market</title>
+    <title>Hunter Net</title>
     <link rel="stylesheet" href="css/fontawesome-all.min.css">
     <link rel="stylesheet" href="css/main.css">
     <?php
